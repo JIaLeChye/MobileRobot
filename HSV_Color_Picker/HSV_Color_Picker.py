a62433d2998fac79655fde01d201a09a3e996d15
JIaLeChye <jialecjl2016@outlook.com>
2024-07-01 09:36:48 +0800

    Updates

--- HSV_Color_Picker/HSV_Color_Picker.py
+++ HSV_Color_Picker/HSV_Color_Picker.py
@@ -1,82 +1,91 @@
-from flask import Flask,Response, render_template, request
+#finding hsv range of target object(pen)
 import cv2
 import numpy as np
-from picamera2 import Picamera2 
+import time
+from picamera2 import Picamera2
+# A required callback method that goes into the trackbar function.
+def nothing(x):
+    pass
 
+# Initializing the webcam feed.
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (640, 480)  # Change resolution if needed
+picam2.preview_configuration.main.format = 'RGB888'
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start() 
 
-app = Flask(__name__, template_folder='Web')
+# Create a window named trackbars.
+cv2.namedWindow("Trackbars")
 
-cam = Picamera2()
-cam.configure(cam.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
-cam.start() 
-
-h_l = 0
-s_l = 179
-v_l = 0 
-
-h_u = 255
-s_u = 0
-v_u = 255 
-
-
-
-def get_frame():
-    frame = cam.capture_array()
-    frame = process_frame(frame)
-    return frame
-
-def process_frame(frame, h_l,s_l,v_l, h_u, s_u, v_u ):
+# Now create 6 trackbars that will control the lower and upper range of 
+# H,S and V channels. The Arguments are like this: Name of trackbar, 
+# window name, range,callback function. For Hue the range is 0-179 and
+# for S,V its 0-255.
+cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
+cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+ 
+while True:
+    
+    # Start reading the webcam feed frame by frame.
+    frame = picam2.capture_array()
+  
+    # Flip the frame horizontally (Not required)
+    frame = cv2.flip( frame, 1 ) 
+    
+    # Convert the BGR image to HSV image.
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_color = np.array([h_l, s_l, v_l])
-    upper_color = np.array([h_u, s_u, v_u])
-    mask = cv2.inRange(hsv, lower_color, upper_color)
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-    return result 
-
-
-def encode_frame():
-    while True: 
-
-        frame = get_frame()
-        h_l, s_l, v_l, h_u, s_u, v_u = color_range()
-        mask = process_frame(frame, h_l, s_l, v_l, h_u, s_u, v_u)
-        ret, buffer = cv2.imencode('.jpg', mask)
-        print("Encoding")
-
-        if ret is not True:
-            raise Exception("Encoding failed")
-        else: 
-            mask = buffer.tobytes()
-            print("Encoding Sucess")
-        yield (b'--frame\r\n'
-            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+    # Get the new values of the trackbar in real time as the user changes 
+    # them
+    l_h = cv2.getTrackbarPos("L - H", "Trackbars")
+    l_s = cv2.getTrackbarPos("L - S", "Trackbars")
+    l_v = cv2.getTrackbarPos("L - V", "Trackbars")
+    u_h = cv2.getTrackbarPos("U - H", "Trackbars")
+    u_s = cv2.getTrackbarPos("U - S", "Trackbars")
+    u_v = cv2.getTrackbarPos("U - V", "Trackbars")
+ 
+    # Set the lower and upper HSV range according to the value selected
+    # by the trackbar
+    lower_range = np.array([l_h, l_s, l_v])
+    upper_range = np.array([u_h, u_s, u_v])
+    
+    # Filter the image and get the binary mask, where white represents 
+    # your target color
+    mask = cv2.inRange(hsv, lower_range, upper_range)
+ 
+    # You can also visualize the real part of the target color (Optional)
+    res = cv2.bitwise_and(frame, frame, mask=mask)
+    
+    # Converting the binary mask to 3 channel image, this is just so 
+    # we can stack it with the others
+    mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    
+    # stack the mask, orginal frame and the filtered result
+    stacked = np.hstack((mask_3,frame,res))
+    
+    # Show this stacked frame at 40% of the size.
+    cv2.imshow('Trackbars',cv2.resize(stacked,None,fx=0.4,fy=0.4))
+    
+    # If the user presses ESC then exit the program
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+    # If the user presses `s` then print this array.
+    if key == ord('s'):
         
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/capture')
-def capture():
-    processed_frame = encode_frame()
-    return Response(processed_frame, mimetype='multipart/x-mixed-replace; boundary=frame')
-
-@app.route('/color_range', methods=['POST'])
-def color_range():
-    h_l = int(request.form['h_l'])
-    s_l = int(request.form['s_l'])
-    v_l = int(request.form['v_l'])
-
-    h_u = int(request.form['h_u'])
-    s_u = int(request.form['s_u'])
-    v_u = int(request.form['v_u'])
-
-    return h_l, s_l, v_l, h_u, s_u, v_u, 'Track Bar Updated'
-
-
-def main():
-    frame = get_frame()
-    process_frame(frame)
-    print("Step 1")
-
-    app.run(host='0.0.0.0', port=5000)
+        thearray = [[l_h,l_s,l_v],[u_h, u_s, u_v]]
+        print(thearray)
+        
+        # Also save this array as penval.npy
+        np.save('hsv_value',thearray)
+        break
+    
+# Release the camera & destroy the windows.
+picam2.stop()
+cv2.destroyAllWindows()
