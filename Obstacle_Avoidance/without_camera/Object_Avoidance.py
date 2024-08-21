f90bfe3a7168c3567c6c6d79fa60f7684276e1b1
JIaLeChye <jialecjl2016@outlook.com>
2024-08-22 02:35:29 +0800

    update coding

--- Obstacle_Avoidance/without_camera/Object_Avoidance.py
+++ Obstacle_Avoidance/without_camera/Object_Avoidance.py
@@ -1,22 +1,17 @@
-import threading 
-import cv2 
-from picamera2 import Picamera2
 from Ultrasonic_sens import Ultrasonic 
 from PCA9685_MC import Motor_Controller
 from Motor_Encoder import Encoder 
 import time 
-from libcamera import controls
+import __main__
+
 
 
 def init():
-    global enc, ultrasonic, Motor, Speed, rotation_speed, threshold, min_thresh_dist, picam2, frame_lock, shutdown_event, isInit
+    global enc, ultrasonic, Motor, Speed, rotation_speed, threshold, min_thresh_dist, isInit
     isInit = False
 
     if not isInit: 
-        picam2 = Picamera2() 
-        picam2.configure(picam2.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
-        picam2.start()
-        picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+        
         enc = Encoder(debug=True)
         ultrasonic = Ultrasonic(debug=True)
         Motor = Motor_Controller()
@@ -24,22 +19,7 @@ def init():
         rotation_speed = 50
         threshold = 30 
         min_thresh_dist = 10 
-        frame_lock = threading.Lock()
-        shutdown_event = threading.Event()
         isInit = True 
-
-
-# Capture and Display Frame 
-def capture_frame():
-    global picam2, frame_lock, shutdown_event
-    while not shutdown_event.is_set():
-        with frame_lock:
-            frame = picam2.capture_array()
-        cv2.imshow("Frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            shutdown_event.set()  # Signal to stop the program
-            break 
-    cv2.destroyAllWindows()  # Cleanup the OpenCV windows
 
 
 def obstacle_Avoid(left, front, right):
@@ -91,44 +71,34 @@ def obstacle_Avoid(left, front, right):
 
 
 def main(): 
-    global enc, ultrasonic, Motor, Speed, rotation_speed, threshold, min_thresh_dist, picam2, shutdown_event
-    
-    init()
-    print("Program Started")
-    
-    capture_thread = threading.Thread(target=capture_frame)  # Start the capture frame thread
-    capture_thread.start()
-    
-    try:
-        while not shutdown_event.is_set():
-            enc.encoder()
-            left, front, right = ultrasonic.distances()
+    global enc, ultrasonic, Motor, Speed, rotation_speed, threshold, min_thresh_dist
+    init() 
+    while True: 
+        enc.encoder()
+        left, front, right = ultrasonic.distances()
+        time.sleep(0.1)
+        if left is not None and front is not None and right is not None: 
+            print("Left: {:.2f}".format(left)) 
+            print("Front: {:.2f}".format(front))
+            print("Right: {:.2f}".format(right)) 
+            print(" ")
+            obstacle_Avoid(left, front, right)
             time.sleep(0.1)
-            if left is not None and front is not None and right is not None: 
-                print("Left: {:.2f}".format(left)) 
-                print("Front: {:.2f}".format(front))
-                print("Right: {:.2f}".format(right)) 
-                print(" ")
-                obstacle_Avoid(left, front, right)
-                time.sleep(0.1)
-            else:
-                print("No data received")
-                Motor.Brake()
-                time.sleep(1)
-
-    except KeyboardInterrupt:
-        shutdown_event.set()
-        Motor.Brake()
-        enc.stop()
-        print("Program Terminated")
-
-    finally: 
-        shutdown_event.set()
-        enc.stop()
-        capture_thread.join()
-        cv2.destroyAllWindows()
-        print("Program Terminated")
+        else:
+            print("No data received")
+            Motor.Brake()
+            time.sleep(1)
 
 
-if __name__ == "__main__":
-    main()
+try:
+    if __main__ == "__main__":
+        main()
+
+except KeyboardInterrupt:
+    Motor.Brake()
+    enc.stop()
+    print("Program Terminated")
+finally:
+    enc.stop()
+    print("Program Terminated")
+
