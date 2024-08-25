717351c3eb592bbe6d4f86733be5ed52e7808680
JIaLeChye <jialecjl2016@outlook.com>
2024-08-25 21:06:02 +0800

    update codings

--- Line_Following/With_Sensors/Line_Following.py
+++ Line_Following/With_Sensors/Line_Following.py
@@ -3,10 +3,10 @@ from Motor_Encoder import Encoder
 import time 
 import RPi.GPIO as GPIO 
 
-def int():  
+def init():  
     global Motor, enc, D1, D2, D3, D4, D5
     Motor = Motor_Controller()
-    enc = Encoder() 
+    enc = Encoder(ODISPLAY = True) 
     GPIO.setmode(GPIO.BCM)
     D1 = 6 
     D2 = 11
@@ -23,9 +23,41 @@ def int():
 
 
 def main():
-    int()
+    global Motor, enc, D1, D2, D3, D4, D5
+    init()
+    while True: 
+        enc.encoder()
+        outerRight = GPIO.input(D1)
+        Right = GPIO.input(D2)
+        center =  GPIO.input(D3)
+        Left  = GPIO.input(D4)
+        outerLeft = GPIO.input(D5) 
+        if outerRight == 0 and Right == 0 and center == 0 and Left == 0 and outerLeft == 0:
+            Motor.Brake()
 
+        if (outerRight == 1 or Right == 1) and center == 0 and Left == 0 and outerLeft == 0:
+            if outerRight == 1 and Right == 0:
+                Motor.Clock_Rotate(40)  # Turn faster if only outerRight is detected
+            else:
+                Motor.Clock_Rotate(20)  # Normal clockwise rotation
 
+        if (outerLeft == 1 or Left == 1) and center == 0 and Right == 0 and outerRight == 0:
+            if outerLeft == 1 and Left == 0:
+                Motor.AntiClock_Rotate(40)  # Turn faster if only outerLeft is detected
+            else:
+                Motor.AntiClock_Rotate(20)  # Normal anticlockwise rotation
+
+        if (center == 1 and Right == 1 and Left == 1) or (center == 1 and Right == 0 and Left == 0 and outerRight == 0 and outerLeft == 0):
+            Motor.Forward(30)
+
+        if center == 1 and Right == 1 and Left == 1 and outerRight == 1 and outerLeft == 1:
+            Motor.Brake()
+
+        if center == 0 and Right == 0 and Left == 0 and (outerRight == 1 or outerLeft == 1):
+            if outerRight == 1 and outerLeft == 0:
+                Motor.Clock_Rotate(40)  # Prioritize turning clockwise with higher speed
+            elif outerLeft == 1 and outerRight == 0:
+                Motor.AntiClock_Rotate(40)  # Prioritize turning anticlockwise with higher speed
 
 try: 
     if __name__ == 'main': 
@@ -33,4 +65,4 @@ try:
 
 except KeyboardInterrupt: 
     Motor.cleanup()
-
+    enc.stop()
--- Object_Tracking/Object_tracking.py
+++ Object_Tracking/Object_tracking.py
@@ -1,2 +1,11 @@
 import cv2 
 import time 
+from picamera2 import Picamera2 
+from libcamera import controls
+
+def init(): 
+    global picam 
+    picam = Picamera2()
+    picam.configure(picam.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+    picam.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+    
