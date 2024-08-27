41aac610705214f2fde59bd12acdd193a518e29b
JIaLeChye <jialecjl2016@outlook.com>
2024-08-27 11:09:57 +0800

    Update Documentation

--- Obstacle_Avoidance/without_camera/Obstacle_Avoidance.py
+++ Obstacle_Avoidance/without_camera/Obstacle_Avoidance.py
@@ -2,7 +2,6 @@ from Ultrasonic_sens import Ultrasonic
 from PCA9685_MC import Motor_Controller
 from Motor_Encoder import Encoder 
 import time 
-import __main__
 
 
 
@@ -16,11 +15,12 @@ def init():
         ultrasonic = Ultrasonic(debug=True)
         Motor = Motor_Controller()
         Speed = 20
-        rotation_speed = 50
+        rotation_speed = 15
         threshold = 30 
         min_thresh_dist = 10 
         isInit = True 
-
+    else: 
+        pass
 
 def obstacle_Avoid(left, front, right):
     global enc, ultrasonic, Motor, Speed, rotation_speed, threshold, min_thresh_dist
@@ -57,17 +57,13 @@ def obstacle_Avoid(left, front, right):
         time.sleep(1)
         Motor.Brake()
     
-    else:
-        if right > threshold and left > threshold:
+    elif right < threshold and left < threshold and front > threshold :
             Motor.Forward(Speed)
-        elif right > threshold:
-            Motor.Clock_Rotate(rotation_speed)
             time.sleep(0.1)
             Motor.Brake()
-        elif left > threshold:
-            Motor.AntiClock_Rotate(rotation_speed)
-            time.sleep(0.1)
-            Motor.Brake()
+    else:
+        Motor.Brake()
+            
 
 
 def main(): 
@@ -91,7 +87,7 @@ def main():
 
 
 try:
-    if __main__ == "__main__":
+    if __name__ == "__main__":
         main()
 
 except KeyboardInterrupt:
@@ -100,5 +96,6 @@ except KeyboardInterrupt:
     print("Program Terminated")
 finally:
     enc.stop()
+    Motor.cleanup()
     print("Program Terminated")
 
