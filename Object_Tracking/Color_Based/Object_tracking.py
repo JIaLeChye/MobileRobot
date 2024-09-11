ba346bddcc360da6e7dd629dfa06f16aaa0bf57b
JIaLeChye <jialecjl2016@outlook.com>
2024-09-11 14:58:41 +0800

    Updated from RGB to RGBA

--- Object_Tracking/Color_Based/Object_tracking.py
+++ Object_Tracking/Color_Based/Object_tracking.py
@@ -44,10 +44,13 @@ def colorPicker():
         u_v = cv2.getTrackbarPos("Upper Value", "Color_Picker")
         lower_bound = np.array([l_h, l_s, l_v])
         upper_bound = np.array([u_h, u_s, u_v])
+
+
         mask = cv2.inRange(hsv, lower_bound, upper_bound) 
+
         res = cv2.bitwise_and(img, img, mask=mask)
-        mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        stacked = np.hstack((mask_3, img, res))
+        mask_3 = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+        stacked = np.hstack((mask, img, res))
         cv2.imshow("Color_Picker", cv2.resize(stacked, None, fx=0.4, fy=0.4))
         if cv2.waitKey(1) & 0xFF == ord('s'):
             break
