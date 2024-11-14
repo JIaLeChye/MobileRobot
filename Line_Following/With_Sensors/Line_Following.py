156c6571bff7a54ffdd985959460ef3f2260cedb
JiaLe Chye <jialecjl2016@outlook.com>
2024-11-15 01:35:58 +0800

    Update Line following with binary logic

--- Line_Following/With_Sensors/Line_Following.py
+++ Line_Following/With_Sensors/Line_Following.py
@@ -10,25 +10,25 @@ Motor = RobotController()
 def main():
     while True: 
         Line_Sensor = Motor.read_line_sensors()
-        outerRight = Line_Sensor[0]
-        Right = Line_Sensor[1]
-        center =  Line_Sensor[2]
-        Left  = Line_Sensor[3]
-        outerLeft = Line_Sensor[4]
+        outerRight = (Line_Sensor >> 4) & 1
+        Right = (Line_Sensor >> 3) & 1
+        center = (Line_Sensor >> 2) & 1
+        Left = (Line_Sensor >> 1) & 1
+        outerLeft = Line_Sensor & 1
         if outerRight == 0 and Right == 0 and center == 0 and Left == 0 and outerLeft == 0:
             Motor.Brake()
             print("Brake")
 
         if (outerRight == 1 or Right == 1) and center == 0 and Left == 0 and outerLeft == 0:
             print("Turn Right")  # Normal clockwise rotation
-            Motor.move(speed=0, turn=-20)
+            Motor.move(speed=0, turn=30)
 
         if (outerLeft == 1 or Left == 1) and center == 0 and Right == 0 and outerRight == 0:
-            Motor.move(speed=0, turn=20)
+            Motor.move(speed=0, turn=-30)
             print("Turn Left")  # Normal anticlockwise rotation
 
         if (center == 1 and Right == 1 and Left == 1 and outerLeft == 0 and outerRight == 0) or (center == 1 and Right == 0 and Left == 0 and outerRight == 0 and outerLeft == 0):
-            Motor.Forward(15)
+            Motor.Forward(40)
             print("Forward")
 
         if center == 1 and Right == 1 and Left == 1 and outerRight == 1 and outerLeft == 1:
