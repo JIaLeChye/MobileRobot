9c3134331d7e69624ec5d897580b55db6e34f3c3
Jiale Chye <jialecjl2016@outlook.com>
2025-03-06 00:20:07 +0800

    Create setup.py

---
+++ RPi_Robot_Hat_Lib/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='RPi_Robot_Hat_Lib',
+    version='1.0.0',
+    py_modules=['RPi_Robot_Hat_Lib'],  # Use packages=['RPI_HAT_Lib'] if it's a package
+    install_requires=[],  # Add dependencies if any
+)
